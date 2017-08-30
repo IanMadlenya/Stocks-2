e8bdf27927da61a606ccd24185c6eb8c2c8bfa09
Matt Stone <matthewstone@usa.com>
2017-08-29 20:08:05 -0400

    Added the current price and updated UI
    Display current price and scrubbed value in the same TextView.
    Moved text above the graph and added US currency format.
    Added navigation bar color and other small UI changes.

--- app/src/main/java/io/github/mattstoney/stocks/MainActivity.java
+++ app/src/main/java/io/github/mattstoney/stocks/MainActivity.java
@@ -14,14 +14,17 @@ import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private StockHistoryAdapter adapter;
-    private TextView scrubValue;
+    private TextView stockPrice;
     private SparkView stockHistoryGraph;
-	private Stock stock;
+    private Stock stock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 try  {
                     stockHistoryGraph = (SparkView) findViewById(R.id.stockhistorygraph);
-                    
-					// Create YahooFinance QCOM stock object as sample
+
+                    // Create YahooFinance QCOM stock object as sample
                     stock = YahooFinance.get("QCOM");
 
                     // Set starting date for stock history
@@ -46,16 +49,29 @@ public class MainActivity extends AppCompatActivity {
                     adapter = new StockHistoryAdapter(stock, from);
 
                     // Gets the TextView object from the activity_main...
-                    scrubValue = (TextView) findViewById(R.id.scrubValue);
+                    stockPrice = (TextView) findViewById(R.id.stockPrice);
+
+                    BigDecimal bdStockPrice = stock.getQuote().getPrice();
+                    String strStockPrice = NumberFormat.getCurrencyInstance(Locale.US).format(bdStockPrice);
+
+                    // Sets the TextView object
+                    stockPrice.setText(strStockPrice);
 
                     // A listener for when the spark view is scrubbed...
                     stockHistoryGraph.setScrubListener(new SparkView.OnScrubListener() {
                         @Override
                         public void onScrubbed(Object value) {
                             // When the scrubber is not scrubbing set the value to an empty string...
-                            if(value == null) scrubValue.setText("");
-                                // Sets the value when the value is not null...
-                            else scrubValue.setText(value.toString());
+                            if(value == null) {
+                                BigDecimal bdStockPrice = stock.getQuote().getPrice();
+                                String strStockPrice = NumberFormat.getCurrencyInstance(Locale.US).format(bdStockPrice);
+                                stockPrice.setText(strStockPrice);
+                            }
+                            // Sets the value when the value is not null...
+                            else {
+                                String strStockPrice = NumberFormat.getCurrencyInstance(Locale.US).format(value);
+                                stockPrice.setText(strStockPrice);
+                            }
                         }
                     });
 
